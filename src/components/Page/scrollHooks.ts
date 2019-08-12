3ce17bb527d1e6a05487570390ff8a67103f9b4b
nossbigg <chenggibson@gmail.com>
2019-08-12 18:15:42 +0800

    Add scroll to ccc ref element

--- src/components/Page/scrollHooks.ts
+++ src/components/Page/scrollHooks.ts
@@ -1,5 +1,6 @@
+import { CCCRefElement } from './../../store/cccTypedefs'
 import { createRef, useState, useEffect } from 'react'
-import { PageNode } from 'store/cccTypedefs'
+import { PageNode, PageParagraph } from 'store/cccTypedefs'
 import queryString from 'query-string'
 
 export interface RefsMap {
@@ -20,12 +21,17 @@ export const useElementRefsState = (
 
   const paragraphKeys = paragraphs.map((_, index) => getParagraphRefKey(index))
   const footnoteKeys = Object.keys(footnotes).map(getFootnoteRefKey)
+  const cccRefToParagraphMapper = createCCCRefToParagraphMapper(paragraphs)
 
   const [currentFocusedElement, setCurrentFocusedElement] = useState(
     getCurrentFocusedElement(locationSearch)
   )
   const [elementRefs, setElementRefs] = useState(
-    createItemRefs([...paragraphKeys, ...footnoteKeys], currentFocusedElement)
+    createItemRefs(
+      [...paragraphKeys, ...footnoteKeys],
+      currentFocusedElement,
+      cccRefToParagraphMapper
+    )
   )
 
   useEffect(() => {
@@ -33,31 +39,64 @@ export const useElementRefsState = (
       return
     }
 
-    const focusedElement = elementRefs[currentFocusedElement]
+    const resolvedElementKey = resolveFocusedElementKey(
+      cccRefToParagraphMapper,
+      currentFocusedElement
+    )
+    const focusedElement = elementRefs[resolvedElementKey]
     if (!focusedElement) {
       setCurrentFocusedElement('')
       return
     }
 
     scrollToElement(focusedElement)
-    setElementRefs(resetElementRefs(elementRefs, currentFocusedElement))
+    setElementRefs(resetElementRefs(elementRefs, resolvedElementKey))
     setCurrentFocusedElement('')
-  }, [currentFocusedElement, elementRefs])
+  }, [currentFocusedElement, elementRefs, cccRefToParagraphMapper])
 
   return elementRefs
 }
 
-const createItemRefs = (keys: string[], focusedElementKey: string): RefsMap =>
-  keys.reduce(
+const createItemRefs = (
+  keys: string[],
+  focusedElementKey: string,
+  cccRefToParagraphMapper: CCCRefToParagraphMapper
+): RefsMap => {
+  const resolvedKey = resolveFocusedElementKey(
+    cccRefToParagraphMapper,
+    focusedElementKey
+  )
+  return keys.reduce(
     (acc, currentKey) => ({
       ...acc,
       [currentKey]: {
         ref: createRef(),
-        highlighted: currentKey === focusedElementKey,
+        highlighted: currentKey === resolvedKey,
       },
     }),
     {}
   )
+}
+
+interface CCCRefToParagraphMapper {
+  [cccRef: string]: string
+}
+
+const createCCCRefToParagraphMapper = (
+  paragraphs: PageParagraph[]
+): CCCRefToParagraphMapper =>
+  paragraphs.reduce((acc, curr, index) => {
+    const cccRefs = curr.elements.filter(e => e.type === 'ref-ccc')
+    if (cccRefs.length === 0) {
+      return acc
+    }
+
+    const cccRef = cccRefs[0] as CCCRefElement
+    return {
+      ...acc,
+      [getCCCRefKey(cccRef.ref_number)]: getParagraphRefKey(index),
+    }
+  }, {})
 
 const resetElementRefs = (
   elementRefs: RefsMap,
@@ -74,6 +113,17 @@ export const getParagraphRefKey = (index: number) => `paragraph-${index + 1}`
 
 export const getFootnoteRefKey = (footnoteNumber: string) =>
   `footnote-${footnoteNumber}`
+
+const getCCCRefKey = (refNumber: number) => `ccc-${refNumber}`
+
+const resolveFocusedElementKey = (
+  cccRefToParagraphMapper: CCCRefToParagraphMapper,
+  currentFocusedElement: string
+): string => {
+  return currentFocusedElement in cccRefToParagraphMapper
+    ? cccRefToParagraphMapper[currentFocusedElement]
+    : currentFocusedElement
+}
 
 const getCurrentFocusedElement = (locationSearch: string): string => {
   const params = queryString.parse(locationSearch)
