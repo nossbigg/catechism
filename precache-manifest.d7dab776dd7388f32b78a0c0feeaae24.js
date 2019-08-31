981d3fe27c2eff91dfb3479544e1277f79abf41a
nossbigg <chenggibson@gmail.com>
2019-08-31 12:51:23 +0800

    Updates

---
+++ precache-manifest.d7dab776dd7388f32b78a0c0feeaae24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740d0093ddfd0f0ac2fd438d111ae7a6",
+    "url": "/catechism/index.html"
+  },
+  {
+    "revision": "929e1475a18039693147",
+    "url": "/catechism/static/css/main.df1fe396.chunk.css"
+  },
+  {
+    "revision": "b5c9f305ba8f407316d2",
+    "url": "/catechism/static/js/2.b0a6d8d8.chunk.js"
+  },
+  {
+    "revision": "929e1475a18039693147",
+    "url": "/catechism/static/js/main.a85123d8.chunk.js"
+  },
+  {
+    "revision": "89ee3beb61e71ed07140",
+    "url": "/catechism/static/js/runtime~main.3c531b9b.js"
+  }
+]);
