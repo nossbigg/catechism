0aab4eec2645cc03fde551b91519312e502253c7
nossbigg <chenggibson@gmail.com>
2019-09-07 20:00:12 +0800

    Call only one http request for each page load

--- src/components/Page/pageHooks.ts
+++ src/components/Page/pageHooks.ts
@@ -4,22 +4,14 @@ import { PUBLIC_FOLDER_URL } from 'components/common/config'
 import { LeanPageNode } from './../../makeStaticAssets/typedefs'
 
 export const useLoadPageContentHook = (shortUrl: string) => {
-  const [lastShortUrl, setShortUrl] = useState(shortUrl)
+  const [lastShortUrl, setShortUrl] = useState('')
   const [pageNode, setPageNode] = useState<LeanPageNode | undefined>(undefined)
 
   useEffect(() => {
-    if (!pageNode || lastShortUrl !== shortUrl) {
+    if (urlHasChanged(lastShortUrl, shortUrl)) {
+      setShortUrl(shortUrl)
       setPageNode(undefined)
-      const loadPageContent = async () => {
-        const data = await request({
-          uri: `${PUBLIC_FOLDER_URL}/ccc/pages/${shortUrl}.json`,
-          method: 'GET',
-          json: true,
-        })
-        setShortUrl(shortUrl)
-        setPageNode(data)
-      }
-      setImmediate(loadPageContent)
+      setImmediate(() => loadPageContent(shortUrl, data => setPageNode(data)))
     }
   }, [shortUrl, pageNode, lastShortUrl])
 
@@ -27,4 +19,18 @@ export const useLoadPageContentHook = (shortUrl: string) => {
     isLoading: !pageNode,
     pageNode,
   }
+}
+
+const urlHasChanged = (url1: string, url2: string) => url1 !== url2
+
+const loadPageContent = async (
+  shortUrl: string,
+  onDataLoad: (data: LeanPageNode) => void
+) => {
+  const data: LeanPageNode = await request({
+    uri: `${PUBLIC_FOLDER_URL}/ccc/pages/${shortUrl}.json`,
+    method: 'GET',
+    json: true,
+  })
+  onDataLoad(data)
 }
