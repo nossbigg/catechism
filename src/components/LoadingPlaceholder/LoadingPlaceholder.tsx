8b2cd0aaa56f01bf24e211718494807cf8fe5944
nossbigg <chenggibson@gmail.com>
2019-09-10 00:01:03 +0800

    Improve loading placeholder with animated ccc logo

--- src/components/LoadingPlaceholder/LoadingPlaceholder.tsx
+++ src/components/LoadingPlaceholder/LoadingPlaceholder.tsx
@@ -1,12 +1,24 @@
 import React from 'react'
-import { makeStyles, CircularProgress } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core'
+import { PUBLIC_FOLDER_URL } from 'components/common/config'
+
+const cccIconPath = `${PUBLIC_FOLDER_URL}/ccc.svg`
 
 export const LoadingPlaceholder: React.FC = () => {
   const styles = useStlyes()
 
   return (
     <div className={styles.container}>
-      <CircularProgress />
+      <div className={styles.cccIconContainer}>
+        <div className={styles.cccIconPlaceholder}>
+          <img
+            src={cccIconPath}
+            height='500px'
+            width='auto'
+            alt='Catechism Icon'
+          />
+        </div>
+      </div>
     </div>
   )
 }
@@ -17,5 +29,23 @@ const useStlyes = makeStyles({
     height: '100vh',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  '@keyframes cccIconAnimation': {
+    '0%': { backgroundColor: 'rgb(215,202,47)' },
+    '50%': { backgroundColor: 'rgb(255,245,129)' },
+    '100%': { backgroundColor: 'rgb(215,202,47)' },
+  },
+  cccIconContainer: {
+    backgroundColor: 'rgb(215,202,47)',
+    maskImage: `url(${cccIconPath})`,
+    animationName: '$cccIconAnimation',
+    animationDuration: '0.75s',
+    animationIterationCount: 'infinite',
+    animationTimingFunction: 'ease-in-out',
+    maskSize: 'contain',
+    maskPosition: 'center',
+  },
+  cccIconPlaceholder: {
+    opacity: 0,
   },
 })
