05b998cb890cdcf109f690e798ca569202c929e0
nossbigg <chenggibson@gmail.com>
2020-07-19 15:44:20 +0800

    Add initial gatsby entrypoint

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  pathPrefix: '/catechism',
+  siteMetadata: {
+    siteName: `Catechism of the Catholic Church`,
+    exampleUrl: `https://nossbigg.github.io/catechism`,
+  },
+  plugins: [],
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Index: React.FC = () => {
+  return 'hello world'
+}
+
+export default Index
